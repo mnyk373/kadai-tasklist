bdb2055704221ffbbccbca931672be62e6a7bc15
mnyk373 <mnyk3730208@gmail.com>
2021-03-28 02:51:39 +0000

    add status

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -10,7 +10,11 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>メッセージ</th>
+            <th>status</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
     </table>
